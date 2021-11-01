2482ed8054c5a536c70218b05189cf4808bd2e20
antjamespadgett <padgettanthonyj@gmail.com>
2021-11-01 11:07:04 -0500

    did the exercise

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SyntaxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var answer = 4;  //explcit typing--
+            //string response;
+
+            //if (answer <12)
+            // {
+            //    response = answer + " is less than nine";
+
+            // else
+            // {
+            //    response = answer + " greater than or equal to nine";
+            // }
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            System.Console.WriteLine(response);
+        }
+    }
+
+}
+
